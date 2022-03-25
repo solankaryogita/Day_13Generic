5fb2e7bc1d1bfef58eb41c0d32f2edccda673bf9
PoojaSolankar <poojasolankar09gmail.com>
2022-03-25 20:50:31 +0530

    Using Generic

--- Generic/PrintArray.java
+++ Generic/PrintArray.java
@@ -2,27 +2,13 @@ package com.Bridgelabz.Generic;
 
 public class PrintArray {
 	
-	 public static void toPrint(Integer[] inputArray) {
-		 for(int element : inputArray) {
+	 public static <E> void toPrint(E[] inputArray) {
+		 for(E element : inputArray) {
 			 System.out.println(element);
 		 }
 		 System.out.println();
 	 }
 	 
-	 public static void toPrint(Double[] inputArray) {
-		 for(double element : inputArray) {
-			 System.out.println(element);
-		 }
-		 System.out.println();
-	 }
-	 
-	 public static void toPrint(Character[] inputArray) {
-		 for(char element : inputArray) {
-			 System.out.println(element);
-		 }
-		 System.out.println();
-	 }
-	
 	public static void main(String[] args) {
 	Integer[] intArray = {1, 2, 3, 4, 5};
 	Double[] doubleArray =  {1.1, 2.2, 3.3, 4.4, 5.5};
